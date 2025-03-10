da501686639af9f747dd36deb03aded4323e8b7b
Tricia Santiago <124637649+jalentricia@users.noreply.github.com>
2025-03-09 19:30:08 -0700

    Add files via upload

---
+++ Rscripts/Aim3/gc_IN_core_microbiome.R
@@ -0,0 +1,39 @@
+#### Intraepithelial neoplasia ####
+
+#!/usr/bin/env Rscript
+library(tidyverse)
+library(phyloseq)
+library(microbiome)
+library(ggVennDiagram)
+
+#### Load data ####
+load("gc_final.RData")
+
+#### "core" microbiome ####
+
+# Convert to relative abundance
+gc_RA <- transform_sample_counts(gc_final, fun=function(x) x/sum(x))
+
+# Filter dataset for "Intraepithelial neoplasia" group and age groups
+IN <- subset_samples(gc_RA, `Group` == "Intraepithelial neoplasia (IN)")
+IN_sample_data <- sample_data(IN) #view the filtered data
+
+middle_IN<- subset_samples(IN, `Age_Category`=="Middle Age")
+elderly_IN <- subset_samples(IN, `Age_Category`=="Elderly")
+
+# Determine ASVs are found in more than 70% of samples in each age category
+middle_IN_ASVs <- core_members(middle_IN, detection=0, prevalence = 0.7)
+elderly_IN_ASVs <- core_members(elderly_IN, detection=0, prevalence = 0.7)
+
+# View the ASVs
+tax_table(prune_taxa(young_IN_ASVs,gc_final))
+tax_table(prune_taxa(middle_IN_ASVs,gc_final))
+tax_table(prune_taxa(elderly_IN_ASVs,gc_final))
+
+# Create a list
+
+gc_IN_list <- list("Middle Age" = middle_IN_ASVs, "Elderly" = elderly_IN_ASVs)
+
+# Create a Venn diagram using all the ASVs shared and unique to antibiotic users and non users
+gc_IN_venn <- ggVennDiagram(x = gc_IN_list) + coord_flip()
+ggsave("gc_IN_venn.png", gc_IN_venn)
---
+++ Rscripts/Aim3/gc_gastric_core_microbiome.R
@@ -0,0 +1,40 @@
+#### Gastric Cancer ####
+
+#!/usr/bin/env Rscript
+library(tidyverse)
+library(phyloseq)
+library(microbiome)
+library(ggVennDiagram)
+
+#### Load data ####
+load("gc_final.RData")
+
+#### "core" microbiome ####
+
+# Convert to relative abundance
+gc_RA <- transform_sample_counts(gc_final, fun=function(x) x/sum(x))
+
+# Filter dataset for "Gastric cancer" group and age groups
+gastric <- subset_samples(gc_RA, `Group` == "Gastric cancer (GC)")
+gastric_sample_data <- sample_data(gastric)
+young_gastric <- subset_samples(gastric, `Age_Category`== "Young Adult")
+middle_gastric<- subset_samples(gastric, `Age_Category`=="Middle Age")
+elderly_gastric <- subset_samples(gastric, `Age_Category`=="Elderly")
+
+# Determine ASVs are found in more than 70% of samples in each age group
+young_gastric_ASVs <- core_members(young_gastric, detection=0, prevalence = 0.7)
+middle_gastric_ASVs <- core_members(middle_gastric, detection=0, prevalence = 0.7)
+elderly_gastric_ASVs <- core_members(elderly_gastric, detection=0, prevalence = 0.7)
+
+# View these ASVs
+tax_table(prune_taxa(young_gastric_ASVs,gc_final))
+tax_table(prune_taxa(middle_gastric_ASVs,gc_final))
+tax_table(prune_taxa(elderly_gastric_ASVs,gc_final))
+
+# Create a list
+
+gc_gastric_list <- list("Young Adult" = young_gastric_ASVs, "Middle Age" = middle_gastric_ASVs, "Elderly" = elderly_gastric_ASVs)
+
+# Create a Venn diagram using all the ASVs shared and unique to antibiotic users and non users
+gc_gastric_venn <- ggVennDiagram(x = gc_gastric_list)
+ggsave("gc_gastric_venn.png", gc_gastric_venn)
