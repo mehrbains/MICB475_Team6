7784e2e544fb32653b709abbc385423bd4f38b4a
Tricia Santiago <124637649+jalentricia@users.noreply.github.com>
2025-04-25 21:23:21 -0400

    Update gc_chronic_core_microbiome.R

--- Rscripts/Aim3/gc_chronic_core_microbiome.R
+++ Rscripts/Aim3/gc_chronic_core_microbiome.R
@@ -38,3 +38,44 @@ gc_chronic_list <- list("Young Adult" = young_chronic_ASVs, "Middle Age" = middl
 # Create a Venn diagram using all the ASVs shared and unique to antibiotic users and non users
 gc_chronic_venn <- ggVennDiagram(x = gc_chronic_list)
 ggsave("gc_chronic_venn.png", gc_chronic_venn)
+
+#### Chronic gastritis prevalence = 0.8 ####
+
+#!/usr/bin/env Rscript
+library(tidyverse)
+library(phyloseq)
+library(microbiome)
+library(ggVennDiagram)
+
+#### Load data ####
+load("gc_final.RData")
+
+#### "core" microbiome ####
+
+# Convert to relative abundance
+gc_RA <- transform_sample_counts(gc_final, fun=function(x) x/sum(x))
+
+# Filter dataset for "Chronic gastritis" group and age groups
+chronic <- subset_samples(gc_RA, `Group` == "Chronic gastritis (CG)")
+chronic_sample_data <- sample_data(chronic)
+young_chronic <- subset_samples(chronic, `Age_Category`== "Young Adult")
+middle_chronic<- subset_samples(chronic, `Age_Category`=="Middle Age")
+elderly_chronic <- subset_samples(chronic, `Age_Category`=="Elderly")
+
+# Determine ASVs are found in more than 70% of samples in each age group
+young_chronic_ASVs <- core_members(young_chronic, detection=0, prevalence = 0.8)
+middle_chronic_ASVs <- core_members(middle_chronic, detection=0, prevalence = 0.8)
+elderly_chronic_ASVs <- core_members(elderly_chronic, detection=0, prevalence = 0.8)
+
+# View these ASVs
+tax_table(prune_taxa(young_chronic_ASVs,gc_final))
+tax_table(prune_taxa(middle_chronic_ASVs,gc_final))
+tax_table(prune_taxa(elderly_chronic_ASVs,gc_final))
+
+# Create a list
+
+gc_chronic_list <- list("Young Adult" = young_chronic_ASVs, "Middle Age" = middle_chronic_ASVs, "Elderly" = elderly_chronic_ASVs)
+
+# Create a Venn diagram using all the ASVs shared and unique to antibiotic users and non users
+gc_chronic_venn <- ggVennDiagram(x = gc_chronic_list)
+ggsave("gc_chronic_venn.png", gc_chronic_venn0.8)
